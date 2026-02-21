0e56d301298edd483a67eacdba6a5e558d31cc68
Md. Ratan Chowdhury <ratanchowdhury169@gmail.com>
2026-02-22 00:57:53 +0600

    install vite,react,react router, tailwindcss and daisyui

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+
+        <h1 className='text-3xl'>Vite + React</h1>
+        <button className="btn btn-neutral">Neutral</button>
+        <button className="btn btn-primary">Primary</button>
+      </div>
+
+
+    </>
+  )
+}
+
+export default App
---
+++ src/Router/Router.jsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter } from "react-router";
+import Root from "../Layout/Root";
+import Home from "../Pages/Home";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+     children: [
+      { index: true, Component: Home },
+     ]
+  },
+]);
