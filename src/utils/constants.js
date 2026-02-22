7a9103077406f189931d555f221f30f1c7a94c74
Md. Ratan Chowdhury <ratanchowdhury169@gmail.com>
2026-02-22 20:34:41 +0600

    set admin login

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 // Admin check: এই ইমেইল যিনি লগইন করবেন তাকে Admin হিসেবে ধরা হবে
-export const ADMIN_EMAIL = 'admin@example.com';
+export const ADMIN_EMAIL = 'admin@ifter.com';
 
 export const isAdmin = (user) => Boolean(user?.email === ADMIN_EMAIL);
